04949c97d2ffd445c2ffe70dfc667c3311efa135
akurpios <105107065+akurpios@users.noreply.github.com>
2023-02-27 12:27:35 +0100

    Create AddTeamViewerDevicesList.ps1

---
+++ TeamViewer/AddTVDeviceList/AddTeamViewerDevicesList.ps1
@@ -0,0 +1,18 @@
+cls
+$csvFile = "C:\Users\aleksander.kurpios\temp\TeamViewerDevicesTEST.csv"
+$tvAccessToken = read-host "Please type your token "
+
+ $headers = @{}
+$headers["Accept"] = "application/json"
+$headers["Authorization"] = "Bearer $tvAccessToken"
+ 
+
+  Import-CSV $csvFile | Foreach-Object {
+curl -Method Post -ContentType application/json -Headers $headers -Body "{
+  'alias': '$($_.alias)', 
+  'groupid': '$($_.groupid)', 
+  'remotecontrol_id': '$($_.remotecontrol_id)', 
+  'description': '$($_.description)', 
+  'password': '$($_.password)' 
+}" 'https://webapi.teamviewer.com/api/v1/devices'
+}
