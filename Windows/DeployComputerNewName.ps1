92a77e44dbfa21948e2919db0eccf87693a0ab9e
Aleksander Kurpios <105107065+akurpios@users.noreply.github.com>
2023-06-26 14:11:14 +0200

    Update DeployComputerNewName.ps1

--- Windows/DeployComputerNewName.ps1
+++ Windows/DeployComputerNewName.ps1
@@ -1,3 +1,4 @@
 $DUID=(Get-Random -Minimum 0 -Maximum 99999).ToString('00000')
-$serial=(gwmi win32_bios).SerialNumber
-Rename-Computer -NewName "Prefix-$DUID-$serial" -Force
+#$serial=(gwmi win32_bios).SerialNumber
+write-host "NEW HOSTNAME: Prefix-$DUID"
+Rename-Computer -NewName "Prefix-$DUID" -Force
