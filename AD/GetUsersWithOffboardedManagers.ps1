f115b877f8e51030fe5cf0e0e484b005aad678ff
Aleksander Kurpios <105107065+akurpios@users.noreply.github.com>
2023-09-29 13:05:24 +0200

    Update GetUsersWithOffboardedManagers.ps1

--- AD/GetUsersWithOffboardedManagers.ps1
+++ AD/GetUsersWithOffboardedManagers.ps1
@@ -1,1 +1,1 @@
-Get-ADUser -Properties *  -Filter * | Where-Object {$_.Manager -like "*offboarded*"} | select Name, SamAccountName, Manager | Export-Csv C:\Users\akurpios\Desktop\UsersWithOffboardedManagers.csv -Encoding UTF8 -NoTypeInformation
+Get-ADUser -Properties *  -Filter * | Where-Object {$_.Manager -like "*offboarded*" -and $_.Name -notlike "*offboarded*"} | select Name, SamAccountName, Manager | Export-Csv C:\Users\akurpios\Desktop\UsersWithOffboardedManagers.csv -Encoding UTF8 -NoTypeInformation
