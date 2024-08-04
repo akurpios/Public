2bd6bd916deaad614e3c50e021a2684bac0ac20c
Aleksander Kurpios <105107065+akurpios@users.noreply.github.com>
2024-08-04 11:11:15 +0200

    Update RequireUserToUpdatePasswordAndMFA.ps1
    Final file more perspicuous

--- AzureAD/RequireUserToUpdatePasswordAndMFA.ps1
+++ AzureAD/RequireUserToUpdatePasswordAndMFA.ps1
@@ -1,83 +1,120 @@
-#Main Script
-$CurrentDate = get-date -f dd-MM-yyyy_THH-mm-ss
-$TranscriptPatch = "C:\Support\Scripts\Revoke Sessions\REPORT$CurrentDate.txt"
+
+#Wipe terminal
+cls
+
+#Echo
+echo "----------------------------------"
+echo "EntraID SignIn Logs VPN checker for VPNAPI.IO"
+echo "By Aleksander Kurpios"
+echo "----------------------------------"
+
+
+#Create Varibles
+$CurrentDate = get-date -f dd-MM-yyyy_THH-mm-ss #Get Current date and time
+$AllIPsArray = @() # wipe All IPs Array table
+$UniqueIPsArray = @() # wipe Unique IPs Array table
+$APIKey = Read-Host -Prompt "Enter your VPNAPI.io API Key: " #Prompt for VPNAPI.io API Key
+
+$MScsvPath = Read-Host -Prompt "Enter your Microsoft report CSV path without quotes: " #Prompt for RAW InteractiveSignIns export file
+$RootdirectoryPath = Split-Path -Path $MScsvPath #Get RAW InteractiveSignIns file location
+New-Item -Path $RootdirectoryPath -Name $CurrentDate -ItemType "directory" #Create new folder for this job
+$directoryPath = "$RootdirectoryPath\$CurrentDate" #Set work folder
+Write-host "`n`nDirectory path: " $directoryPath "`n`n" #Display work folder
+$TempMScsvPath = "$directoryPath\TempRaw-$CurrentDate.csv" #Set location for Temp CSV
+$TranscriptPatch = "$directoryPath\MsLogsVPNchecker-REPORT$CurrentDate.txt" #Set location for transcript
+$RawOutPath = "$directoryPath\TempResolvedIPs-$CurrentDate.csv"
+$UniqueTempMScsvPath = "$directoryPath\UniqueTempRaw-$CurrentDate.csv" #Set location for Temp Unique IPs CSV
+$OutPath = "$directoryPath\ResolvedIPs-$CurrentDate.csv" #Set OutPath
+$Progress = 1 #Wipe Progress bar status
+
+#Start transcript
 Start-Transcript -Path $TranscriptPatch
-$csvPath = Read-Host -Prompt "Enter your CSV path without quotes "
-Import-Csv $csvPath -Delimiter ';' |
 
+# Replace "Incoming token type" with "Token" in a file
+$OldfirstLine = Get-Content -Path $MScsvPath | Select-Object -First 1
+[regex]$pattern = "Incoming token type"
+$NewfirstLine = $pattern.replace($OldfirstLine, "Token", 1) 
 
-ForEach-Object { 
-$UPN = $_.UserPrincipalName
-$SAMA = $_.SamAccountName
+#Replace "IP Address" with "IP" in file
+$NewfirstLine = $NewfirstLine.Replace("IP address","IP")
 
-write-host "Working on account: "$SAMA 
+#Replace 1st line of string
+$x = Get-Content $MScsvPath
+$x[0] = $NewfirstLine
+$x | Out-File $TempMScsvPath
 
-Write-Host "PERFORMING ACTION`r`n"
+#Remove duplicated IPs from Fixed CSV
+Import-Csv $TempMScsvPath | Sort-Object "IP" -Unique | Export-Csv -Path $UniqueTempMScsvPath
 
-try{
-$UserObject = get-aduser $SAMA
-$tempUPN = $UserObject.UserPrincipalName
-Set-ADUser $SAMA -PasswordNeverExpires:$false
-Set-ADUser $SAMA -ChangePasswordAtLogon:$true
-$ObjectID = (Get-AzureADUser -SearchString $tempUPN).ObjectID
-Revoke-AzureADUserAllRefreshToken -ObjectId $ObjectID
+#Checking size of unique IP list
 
-} catch {
+$CountOfUniqueIPs = (Get-Content $UniqueTempMScsvPath | Measure-Object -Line).Lines
+$CountOfUniqueIPs = $CountOfUniqueIPs
 
-Write-Host "ACTION for: "$UPN " not success`r`n" -BackgroundColor Red
+#Checking Unique IPs
+Import-Csv $UniqueTempMScsvPath | ForEach-Object { 
+    $Progress++
+    $Date = $_."Date (UTC)"
+    $IP = $_.IP
+    $Username = $_.Username
+    $URL = "https://vpnapi.io/api/"+$IP+"?key="+$APIKey
+    $Curl = Invoke-RestMethod -Uri $URL
+    write-host "Working for: "$IP
+    echo $Curl
 
+    [PsCustomObject]@{
+        username = $Username;
+        IP = $IP;
+        VPN = $Curl.security.vpn;
+        TOR = $Curl.security.tor;
+        Country = $Curl.location.country;
+        ISP = $Curl.network.autonomous_system_organization;
+
+    } | Export-Csv $RawOutPath -NoTypeInformation -append
+
+     Write-Progress -activity "Checking Unique IPs" -status "Scanned: $Progress of $($CountOfUniqueIPs)" -percentComplete (($Progress / $CountOfUniqueIPs)  * 100)
 }
 
-Write-Host "ACTION for: "$UPN " success`r`n" -BackgroundColor Green
+#Remove duplicates from Exported CSV (Multiple header lines)
+Import-Csv (Get-ChildItem $RawOutPath) | Sort-Object -Unique IP | Export-Csv $OutPath -NoClobber -NoTypeInformation
 
-write-host "*******************************`r`n"
-write-host 
+#Check if Final CSV is correct
+$FinalCSVsize = (Get-Content $OutPath | Measure-Object -Line).Lines
+
+Write-Host "Final file size: " $FinalCSVsize
+Write-Host "Unique entries: " $CountOfUniqueIPs
+
+if ($FinalCSVsize -eq $CountOfUniqueIPs) {
+    Write-Host "Final CSV is correct" -BackgroundColor Green
+}
+else {
+    Write-Host "Final CSV is incorrect" -BackgroundColor Red
 }
 
-Start-ADSyncSyncCycle -PolicyType Delta
+#Ask if user want to keep temp files
+$DeleteTemp = Read-Host "Want to keep temp files [y/n]"
+while($DeleteTemp -ne "y")
+{
+    if ($DeleteTemp -eq 'n') {    
+      Write-Host "Removing temp files" -BackgroundColor DarkYellow
+      Remove-Item -Path $RawOutPath
+      Remove-Item -Path $TempMScsvPath
+      Remove-Item -Path $UniqueTempMScsvPath
+      break
+      }
+  Write-Host "Keeping temp files" -BackgroundColor DarkYellow
+}
+
+#Ask if user want to open export location
+$DeleteTemp = Read-Host "Want to open export location [y/n]"
+while($DeleteTemp -ne "n")
+{
+    if ($DeleteTemp -eq 'y') {  
+        Write-Host "Opening export location" -BackgroundColor DarkYellow  
+        ii $directoryPath
+      }
+      break
+}
 
 Write-Host "Script Finished"
-pause
 Stop-Transcript
-#notepad.exe $TranscriptPatch
-
-#Checker
-$CurrentDate = get-date -f dd-MM-yyyy_THH-mm-ss
-$TranscriptPatch = "C:\Support\Scripts\Revoke Sessions\REPORTChecker$CurrentDate.txt"
-Start-Transcript -Path $TranscriptPatch
-$csvPath = Read-Host -Prompt "Enter your CSV path without quotes "
-$OutputPath = "C:\Support\Scripts\Revoke Sessions\REPORTChecker$CurrentDate.csv"
-Import-Csv $csvPath  -Delimiter ';' |
-
-
-ForEach-Object { 
-$UPN = $_.UserPrincipalName
-$SAMA = $_.SamAccountName
-
-write-host "Working on account: "$UPN
-
-Write-Host "PERFORMING ACTION`r`n"
-
-try{
-
-Get-ADUser -identity $SAMA  -properties * | Select SamAccountName,Name,UserPrincipalName,@{Name='PwdLastSet';Expression={[DateTime]::FromFileTime($_.PwdLastSet)}} | export-csv $OutputPath -Encoding UTF8 -NoTypeInformation -Append
-
-} catch {
-
-Write-Host "ACTION for: "$UPN " not success`r`n" -BackgroundColor Red
-
-}
-
-Write-Host "ACTION for: "$UPN " success`r`n" -BackgroundColor Green
-
-write-host "*******************************`r`n"
-write-host 
-}
-
-Write-Host "Script Finished"
-pause
-Stop-Transcript
-#notepad.exe $TranscriptPatch
-
-
-
