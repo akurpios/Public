2181bff70874724a5a2b66bc92383d72c03f8646
Aleksander Kurpios <105107065+akurpios@users.noreply.github.com>
2024-09-05 10:24:46 +0200

    Update GetEntraIDUsersDetails.ps1

--- AzureAD/GetEntraIDUsersDetails.ps1
+++ AzureAD/GetEntraIDUsersDetails.ps1
@@ -15,7 +15,7 @@ Start-Transcript -Path $TranscriptPatch
 Connect-MgGraph -Scopes "User.Read.All", "AuditLog.Read.All", "Organization.Read.All"
 $producttable = Invoke-RestMethod 'https://gist.githubusercontent.com/krzydoug/84aa5af47335c219f42a820c84c1371d/raw/13de15e8768adb73595ce226259af96510906337/Product%2520Table.psd1' | Invoke-Expression
 
-$UserLastEntraIDLoginData = Get-MgUser -All -Property DisplayName,UserPrincipalName,AccountEnabled,SignInActivity,lastPasswordChangeDateTime | select DisplayName,UserPrincipalName,AccountEnabled,@{N='LastSignInDate';E={$_.SignInActivity.LastSignInDateTime}},lastPasswordChangeDateTime,@{N='License';E={$producttable[(Get-MgUserLicenseDetail -UserId $_.id).skuid] -join '; '}}
+$UserLastEntraIDLoginData = Get-MgBetaUser -All -Property * | select Id,DisplayName,UserPrincipalName,UserType,CompanyName,Department,officeLocation,UsageLocation,preferredDataLocation,preferredLanguage,showInAddressList,AccountEnabled,@{N='LastSignInDate';E={$_.SignInActivity.LastSignInDateTime}},lastPasswordChangeDateTime,@{N='License';E={$producttable[(Get-MgBetaUserLicenseDetail -UserId $_.id).skuid] -join '; '}}, @{N='EA1';E={$_.onPremisesExtensionAttributes.extensionAttribute1}}, @{N='EA5';E={$_.onPremisesExtensionAttributes.extensionAttribute5}}, @{N='EA10';E={$_.onPremisesExtensionAttributes.extensionAttribute10}}, OnPremisesSamAccountName
 
 $UserLastEntraIDLoginData | Export-Csv -Path $ExportPath -Encoding UTF8 -NoTypeInformation -Append
 
